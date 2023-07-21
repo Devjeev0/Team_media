aa15b1537cb4518a337b09bad544fc2d7266821d
Devjeev0 <ajjeeva33@gmail.com>
2023-07-21 11:20:28 +0530

    first commit

--- app/serializers.py
+++ app/serializers.py
@@ -1,79 +1,8 @@
 from rest_framework import serializers
 from .models import Room, Team, Timing
-from datetime import datetime, time, timedelta
+from datetime import datetime, time, timedelta, date
 from django.utils import timezone
 
-#
-# class TeamSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Team
-#         fields = '__all__'
-#         depth = 1
-#
-# class TimingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Timing
-#         fields = '__all__'
-#         depth = 1
-#
-# class RoomSerializer(serializers.ModelSerializer):
-#     timings = TimingSerializer(many=True, read_only=True)
-#     # bookings = TeamSerializer(many=True, read_only=True)
-#     free_slots = serializers.SerializerMethodField()
-#
-#     class Meta:
-#         model = Room
-#         fields = ['id', 'room_name', 'timings', 'free_slots']
-#         depth = 1
-#
-#     def get_free_slots(self, obj):
-#         timings = obj.timings.all().order_by('date', 'time_from')
-#         free_slots = []
-#
-#         # Get the minimum and maximum dates from timings
-#         min_date = timings.first().date if timings.exists() else timezone.now().date()
-#         max_date = timings.last().date if timings.exists() else timezone.now().date()
-#
-#         # Iterate over each date
-#         current_date = min_date
-#         while current_date <= max_date:
-#             time_slots = self.get_time_slots(obj, current_date)
-#             free_slots.extend(time_slots)
-#             current_date += timedelta(days=1)
-#
-#         return free_slots
-#
-#     def get_time_slots(self, obj, date):
-#         timings = obj.timings.filter(date=date).order_by('time_from')
-#         time_slots = []
-#
-#         if not timings.exists():
-#             time_slots.append({
-#                 'date': date,
-#                 'start_time': time.min,
-#                 'end_time': time.max,
-#             })
-#         else:
-#             start_time = time.min
-#             for timing in timings:
-#                 if timing.time_from != start_time:
-#                     time_slots.append({
-#                         'date': date,
-#                         'start_time': start_time,
-#                         'end_time': timing.time_from,
-#
-#                     })
-#                 start_time = timing.time_to
-#
-#             end_time = time.max
-#             if start_time != end_time:
-#                 time_slots.append({
-#                     'date': date,
-#                     'start_time': start_time,
-#                     'end_time': end_time,
-#                 })
-#
-#         return time_slots
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
@@ -87,26 +16,25 @@ class TimingSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 1
 
+
 class RoomSerializer(serializers.ModelSerializer):
     timings = TimingSerializer(many=True, read_only=True)
-    bookings = TeamSerializer(many=True, read_only=True)  # Include the bookings field
+    bookings = TeamSerializer(many=True, read_only=True)  
 
     free_slots = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
-        fields = ['id', 'room_name', 'timings', 'bookings', 'free_slots']  # Include the bookings field
+        fields = ['id', 'room_name', 'timings', 'bookings', 'free_slots',]  
         depth = 1
 
     def get_free_slots(self, obj):
         timings = obj.timings.all().order_by('date', 'time_from')
         free_slots = []
 
-        # Get the minimum and maximum dates from timings
         min_date = timings.first().date if timings.exists() else timezone.now().date()
         max_date = timings.last().date if timings.exists() else timezone.now().date()
 
-        # Iterate over each date
         current_date = min_date
         while current_date <= max_date:
             time_slots = self.get_time_slots(obj, current_date)
@@ -114,6 +42,8 @@ class RoomSerializer(serializers.ModelSerializer):
             current_date += timedelta(days=1)
 
         return free_slots
+    
+
 
     def get_time_slots(self, obj, date):
         timings = obj.timings.filter(date=date).order_by('time_from')
@@ -145,3 +75,4 @@ class RoomSerializer(serializers.ModelSerializer):
                 })
 
         return time_slots
+
--- app/views.py
+++ app/views.py
@@ -10,6 +10,8 @@ from .serializers import *
 #     serializer_class = RoomSerializer
 
 
+from time import strptime
+
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
@@ -17,67 +19,27 @@ class RoomViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         rooms = self.get_queryset()
         serializer = self.get_serializer(rooms, many=True)
-        return render(request, 'table.html', {'rooms': serializer.data})
-# from rest_framework.decorators import api_view
-# from rest_framework import viewsets
-# from rest_framework.response import Response
-#
-# @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-# def room_view(request):
-#     if request.method == 'GET':
-#         queryset = Room.objects.all()
-#         serializer = RoomSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = RoomSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=201)
-#         return Response(serializer.errors, status=400)
-#
-# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-# def room_detail_view(request, pk):
-#     try:
-#         room = Room.objects.get(pk=pk)
-#     except Room.DoesNotExist:
-#         return Response(status=404)
-#
-#     if request.method == 'GET':
-#         serializer = RoomSerializer(room)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT' or request.method == 'PATCH':
-#         serializer = RoomSerializer(room, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=400)
-#     elif request.method == 'DELETE':
-#         room.delete()
-#         return Response(status=204)
+
+        for room_data in serializer.data:
+            for free_slot in room_data['free_slots']:
+                start_time_str = free_slot['start_time']
+                end_time_str = free_slot['end_time']
+
+                start_time_str = start_time_str.strftime("%H:%M:%S")
+                end_time_str = end_time_str.strftime("%H:%M:%S")
+
+                start_time = strptime(start_time_str, "%H:%M:%S")
+                end_time = strptime(end_time_str, "%H:%M:%S")
+
+                duration_seconds = (end_time.tm_hour * 3600 + end_time.tm_min * 60 + end_time.tm_sec) - (start_time.tm_hour * 3600 + start_time.tm_min * 60 + start_time.tm_sec)
+
+                free_slot['duration'] = duration_seconds // 60
+
+        return render(request, 'index.html', {'rooms': serializer.data})
 
 
-# views.py
-
-# views.py
-
-import requests
-from django.shortcuts import render
 
 
-def room_table_view(request):
-    # Fetch the timings data from the API or database
-    try:
-        response = requests.get('http://localhost:8000/rooms/')
-        response.raise_for_status()
-        data = response.json()
 
-        if isinstance(data, list):
-            timings = data
-        else:
-            timings = data.get('timings', [])
-    except (requests.RequestException, ValueError):
-        timings = []
 
-    context = {'timings': timings}
-    return render(request, 'index.html', context)
 
